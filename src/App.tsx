56d011d1151c7ea1b2d1d2b2e4a6a81d6c06c44b
Chris Norman <17420369+chrnorm@users.noreply.github.com>
2020-05-29 17:14:54 +0800

    simple sql editor

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,40 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+import Editor from "react-simple-code-editor";
+import { highlight, languages } from "prismjs/components/prism-core";
+import "prismjs/components/prism-clike";
+import "prismjs/components/prism-sql";
+import "prismjs/components/prism-plsql";
+
+const initialCode = `SELECT home_id, home_type, bathrooms
+FROM homes
+WHERE home_id < 500
+AND home_type = 'two-storey'
+ORDER BY home_type ASC, bathrooms DESC;
+`;
 
 function App() {
+  const [code, setCode] = useState<string>(initialCode);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main className="container">
+      <div className="container__content">
+        <h1>exponent-demo-sql-editor</h1>
+        <p>A simple web-based PLSQL editor with syntax highlighting.</p>
+        <div className="container_editor_area">
+          <Editor
+            value={code}
+            onValueChange={(code) => setCode(code)}
+            highlight={(code) => highlight(code, languages.plsql)}
+            padding={10}
+            style={{
+              fontFamily: '"Fira code", "Fira Mono", monospace',
+              fontSize: 12,
+            }}
+          />
+        </div>
+      </div>
+    </main>
   );
 }
 
